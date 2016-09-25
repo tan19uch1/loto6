057c979cd9554284504490a12c4669324fff0cfa
Taniguchi Shotaro <taniguchi@rotary.kuee.kyoto-u.ac.jp>
2016-09-25 20:25:13 +0900

    add library

--- roto6_RBM.py
+++ roto6_RBM.py
@@ -1,1 +1,13 @@
-test2
+#PREDICTION ROTO 6 by RBM via CHAINER
+
+#LIBRALY
+	import matplotlib.pyplot as plt
+	import numpy as np
+	from sklearn.datasets import fetch_mldata
+	from chainer import cuda, Variable, FunctionSet, optimizers
+	import chainer.functions  as F
+	import sys
+	plt.style.use('ggplot')
+
+#PARAMETER
+
